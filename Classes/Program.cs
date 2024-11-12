ea2ada2a9ab26f2ee30a6be54c971e34c135b55c
Robert Anderson <rganderson0798@gmail.com>
2024-11-12 12:52:54 -0800

    Classes-Exercise

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var carOne = new Car();
+            carOne.Make = "R8";
+            carOne.Model = "Audi";
+            carOne.Year = 2020;
+
+            Console.WriteLine($"This is a {carOne.Make} {carOne.Model}, made in the year {carOne.Year}");
         }
     }
 }
